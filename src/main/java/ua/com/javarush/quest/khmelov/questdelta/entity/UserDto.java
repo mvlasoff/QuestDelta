6cff0cd51d43ffd0b28a0c8ad6acb0c0127fe970
mulasau <mikevdga@gmail.com>
2022-09-30 20:55:18 -0400

    9.30 20:54 Expended UserDto.

--- src/main/java/ua/com/javarush/quest/khmelov/questdelta/entity/UserDto.java
+++ src/main/java/ua/com/javarush/quest/khmelov/questdelta/entity/UserDto.java
@@ -1,14 +1,17 @@
 package ua.com.javarush.quest.khmelov.questdelta.entity;
 
+import ua.com.javarush.quest.khmelov.questdelta.repository.GameStatistics;
+
 public class UserDto {
     private String name;
-
     private Role role;
+    private GameStatistics gameStatistics;
 
 
     public UserDto(User user) {
         this.name = user.getLogin();
         this.role = user.getRole();
+        this.gameStatistics = user.getGameStatistics();
     }
 
     public String getName() {
@@ -17,5 +20,9 @@ public class UserDto {
 
     public Role getRole() {
         return role;
+    }
+
+    public GameStatistics getGameStatistics() {
+        return gameStatistics;
     }
 }
